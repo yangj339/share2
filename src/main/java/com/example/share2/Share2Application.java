6b518cae38df82bcdf359482e0bdac565d650a76
yangj339 <yangj339@gmail.com>
2021-04-14 20:30:41 +0800

    初始化

---
+++ src/main/java/com/example/share2/Share2Application.java
@@ -0,0 +1,13 @@
+package com.example.share2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Share2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Share2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/share2/Share2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.share2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Share2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
